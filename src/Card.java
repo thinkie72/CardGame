67fa83300e7b474550e455bd112e42872456e5d3
thinkie72 <tyler.hinkie@menloschool.org>
2023-11-27 13:48:54 -0800

    Finished card class as per canvas instructions.

--- src/Card.java
+++ src/Card.java
@@ -3,7 +3,37 @@ public class Card {
     private String suit;
     private String rank;
 
-    public Card(String suit){
+    public Card(int point, String suit, String rank){
+        this.point = point;
         this.suit = suit;
+        this.rank = rank;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public String toString() {
+        return rank + " of " + suit;
     }
 }
